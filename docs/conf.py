91a86d12a479fa5fa6ef1ae0cca5992d3cf9db53
BilalDev <fannydeback@gmail.com>
2015-11-12 14:32:29 +0100

    Still not

--- docs/conf.py
+++ docs/conf.py
@@ -256,7 +256,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    (master_doc, 'src', u'HolyScrap Documentation',
+    (master_doc, 'HolyScrap', u'HolyScrap Documentation',
      [author], 1)
 ]
 
@@ -270,8 +270,8 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-  (master_doc, 'src', u'HolyScrap Documentation',
-   author, 'src', 'One line description of project.',
+  (master_doc, 'HolyScrap', u'HolyScrap Documentation',
+   author, 'HolyScrap', 'One line description of project.',
    'Miscellaneous'),
 ]
 
