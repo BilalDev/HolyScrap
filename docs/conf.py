e078e83f07d8937e8a936f53f6f02884211f6aa9
BilalDev <fannydeback@gmail.com>
2015-11-12 14:26:42 +0100

    Doc not found

--- docs/conf.py
+++ docs/conf.py
@@ -50,7 +50,7 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'src'
+project = u'HolyScrap'
 copyright = u'2015, HolyScrap'
 author = u'HolyScrap'
 
@@ -226,7 +226,7 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-  (master_doc, 'src.tex', u'src Documentation',
+  (master_doc, 'src.tex', u'HolyScrap Documentation',
    u'HolyScrap', 'manual'),
 ]
 
@@ -256,7 +256,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    (master_doc, 'src', u'src Documentation',
+    (master_doc, 'src', u'HolyScrap Documentation',
      [author], 1)
 ]
 
@@ -270,7 +270,7 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-  (master_doc, 'src', u'src Documentation',
+  (master_doc, 'src', u'HolyScrap Documentation',
    author, 'src', 'One line description of project.',
    'Miscellaneous'),
 ]
